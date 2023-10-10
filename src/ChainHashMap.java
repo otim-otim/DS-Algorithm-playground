9bd51ffd69f468768437ac21346f2051c3997f20
otim isaac <otim.idb@gmail.com>
2023-10-10 08:58:48 +0300

    implement hashmap put function

---
+++ src/ChainHashMap.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+
+public class ChainHashMap {
+    LinkedList<Entry>[] entries;
+    public ChainHashMap(int arraySize){
+        entries = new LinkedList[arraySize];
+    }
+
+    private int hash(int key){
+        return key % entries.length;
+    }
+
+    public void put(Entry entry){
+        int idx = hash(entry.key);
+        LinkedList<Entry> bucketList = entries[idx];
+        if(bucketList == null) {
+            bucketList.addFirst(entry);
+            return;
+        }
+        //if key exists in bucket
+        for (Entry bucketEntry : bucketList  ) {
+            if(bucketEntry.key == entry.key) {
+                bucketEntry.value = entry.value;
+                return;
+            }
+
+        }
+        bucketList.addLast(entry);
+    }
+
+    public Entry get(int key){
+        int idx = hash(key);
+        return entries[idx];
+    }
+
+
+}
---
+++ src/Entry.java
@@ -0,0 +1,10 @@
+public class Entry {
+    public int key;
+    public String value;
+    public Entry(int key, String string){
+        this.key = key;
+        value = string;
+    }
+
+//    public int getKey()
+}
