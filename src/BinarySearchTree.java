714e34efa531c14e2e470329a8abf745eb129e82
Otim Isaac <isaacomega16@gmail.com>
2024-05-02 09:49:10 +0300

    traverse tree inorder

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -64,4 +64,19 @@ public class BinarySearchTree {
 
 
     }
+
+    public void traverseInOrder(){
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(TreeNode root){
+        if(root == null)
+            return;
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traverseInOrder(root.rightChild);
+
+    }
+
+
 }
